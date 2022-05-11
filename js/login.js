bd11f38246a7b45a7b64d724ad3d67286d381d06
SilviaCastroCaamano25 <silvia.castro.caamano@gmail.com>
2022-05-11 19:12:42 +0200

    Creación del login.js para mover el contenedor delantero

---
+++ js/login.js
@@ -0,0 +1,18 @@
+document.getElementById("btn_register").addEventListener("click", register);
+
+//Variables del formulario
+var contenedor_login_register=document.querySelector(".contenedor_login_register");
+var formulario_login=document.querySelector(".formulario_login");
+var formulario_register=document.querySelector(".formulario_register");
+var caja_trasera_login=document.querySelector(".caja_trasera_login");
+var caja_trasera_register=document.querySelector(".caja_tarsera_register");
+
+function register(){
+    formulario_register.style.display = "block";
+    formulario_login.style.display = "none";
+    
+    contenedor_login_register.style.left = "100px";
+
+    caja_trasera_register.style.opacity = "0";
+    caja_trasera_login.style.opacity = "1";
+}
