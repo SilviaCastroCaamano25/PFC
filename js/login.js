69457f2b356c02bf74d72b235d87d5c6beb1e701
SilviaCastroCaamano25 <silvia.castro.caamano@gmail.com>
2022-05-12 19:23:44 +0200

    Terminada las funciones de register y login con transición

--- js/login.js
+++ js/login.js
@@ -1,11 +1,12 @@
 document.getElementById("btn_register").addEventListener("click", register);
+document.getElementById("btn_login").addEventListener("click", login);
 
 //Variables del formulario
 var contenedor_login_register=document.querySelector(".contenedor_login_register");
 var formulario_login=document.querySelector(".formulario_login");
 var formulario_register=document.querySelector(".formulario_register");
 var caja_trasera_login=document.querySelector(".caja_trasera_login");
-var caja_trasera_register=document.querySelector(".caja_tarsera_register");
+var caja_trasera_register=document.querySelector(".caja_trasera_register");
 
 function register(){
     formulario_register.style.display = "block";
@@ -13,5 +14,16 @@ function register(){
     
     contenedor_login_register.style.left = "410px";
 
+    caja_trasera_register.style.opacity = "0";
     caja_trasera_login.style.opacity = "1";
+}
+
+function login(){
+    formulario_register.style.display = "none";
+    formulario_login.style.display = "block";
+    
+    contenedor_login_register.style.left = "10px";
+
+    caja_trasera_register.style.opacity = "1";
+    caja_trasera_login.style.opacity = "0";
 }
