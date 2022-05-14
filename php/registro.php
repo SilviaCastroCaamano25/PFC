73982de552caa2d9b9cbf8ad28ff58c9167143d2
SilviaCastroCaamano25 <silvia.castro.caamano@gmail.com>
2022-05-14 03:38:41 +0200

    Creación de verificación del correo, usuario para  que no salgan duplicados y de la   contraseña para que tenga cierta longitud

--- php/registro.php
+++ php/registro.php
@@ -16,13 +16,55 @@
     /**
      * Definimos la consulta SQL en la tabla
      */
-    $tusuarios=mysqli_query($db, "INSERT INTO tusuarios (usuario, nombre, apellido1, apellido2, email, contrasenha) VALUES ('$usuario', '$nombre', '$apellido1', '$apellido2', '$email', '$password')") or die ("Fallo en la consulta");
+    $consulta="INSERT INTO tusuarios (usuario, nombre, apellido1, apellido2, email, contrasenha) VALUES ('$usuario', '$nombre', '$apellido1', '$apellido2', '$email', '$password')";
+
+    /**
+     * Verificar que la contraseña no tenga una longitud menor de 8 caracteres y no se guarde en la BBDD
+     */
+    if(strlen($password) < 8){
+        echo '<script>
+                alert("La contraseña debe tener 8 o más caracteres");
+                window.location = "../login.html";
+              </script>';
+        exit();
+    } else {
+        echo 'Error en la verificación de la contraseña';
+    }
+
+    /**
+     * Verificar que el correo no se repita en la BBDD
+     */
+    $verificar=mysqli_query($db, "SELECT * FROM tusuarios WHERE email='$email'");
+    if(mysqli_num_rows($verificar) > 0){
+        echo '<script>
+                alert("Este correo ya está registrado");
+                window.location = "../login.html";
+              </script>';
+        exit();
+    } else {
+        echo 'Error en la verificación del correo';
+    }
+
+    /**
+     * Verificar que el usuario no se repita en la BBDD
+     */
+    $verificar_usuario=mysqli_query($db, "SELECT * FROM tusuarios WHERE usuario='$usuario'");
+    if(mysqli_num_rows($verificar_usuario) > 0){
+        echo '<script>
+                alert("Este usuario ya está registrado");
+                window.location = "../login.html";
+              </script>';
+        exit();
+    } else {
+        echo 'Error en la verificación del usuario';
+    }
 
     /**
      * Si el usuario se registra correctamente me mostrará un alert y te llevará a la página principal,
      * si el usuario no pudo registrarse se mostrará un alert y te llevará a la página del login
      */
-    if($tusuarios){
+    $registra=mysqli_query($db, $consulta);
+    if($registra){
         echo '<script>
                 alert("Usuario Registrado");
                 window.location = "../PaginaPrincipal.html";
@@ -37,6 +79,6 @@
     /**
      * Cerramos la conexión con la BBDD
      */
-    mysqli_free_result ($categoria);
-    mysqli_close ($db);
+
+     mysqli_close ($db);
 ?>
