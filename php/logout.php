76405cd62a927003ad2729f38b34fb86419f3d2c
SilviaCastroCaamano25 <silvia.castro.caamano@gmail.com>
2022-05-24 20:47:41 +0200

    Problema que había en el logout.php solucionado

--- php/logout.php
+++ php/logout.php
@@ -1,17 +1,5 @@
-<?
+<?php
     session_start();
     session_destroy();
     header('Location: ../login_formulario.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="../login_formulario.php">Ir al formulario</a>
-</body>
-</html>
